68a0f3b96449f8185e936376ca408d95c4feba36
mindexpert7546 <Kundan.Kumar@iinterchange.net>
2024-05-03 14:34:23 +0530

    Routes Redirection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,17 @@ import { ProductComponent } from './product/product.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  {path:'', component:HomeComponent},
+  {path:'', redirectTo:'login',pathMatch:'full'},
+  {path:'home', component:HomeComponent},
+  {path:'login', component:LoginComponent}, 
   {path:'product', component:ProductComponent},
   {path:'about', component:AboutComponent},
-  {path:'contact', component:ContactComponent}
+  {path:'contact', component:ContactComponent},
+  {path:'**', component:PageNotFoundComponent}
 ];
 
 @NgModule({
@@ -22,7 +27,9 @@ const routes: Routes = [
     HomeComponent,
     ProductComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
